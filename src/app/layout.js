88a45f3fddea9c8fc4cb81eff926636bf54d8c1f
MrPotterOP <shubham.ubarhande1044@gmail.com>
2025-12-07 14:14:45 +0530

    vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, DM_Serif_Display } from "next/font/google";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/next";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,6 +27,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${dmSerif.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
